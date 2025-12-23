6c336731e3a3dd81ac1c90ce42f90b976fcba3b3
sagar830-ops <sagarpalamwar830@gmail.com>
2025-12-23 19:19:48 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,28 @@
+package com.cjc.model;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.cjc.model.Student;
+import com.cjc.model.HibernateUtil;
+
+public class TestApp {
+
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Student s = new Student();
+        s.setName("Ravi");
+        s.setRollno(111);
+        s.setAddress("Wani");
+
+        session.save(s);
+
+        tx.commit();
+        session.close();
+
+        System.out.println("Data inserted successfully");
+    }
+}
